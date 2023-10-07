99b6486637509492b387bef1dccc855ee331d042
nimishapandey1 <144951514+nimishapandey1@users.noreply.github.com>
2023-10-07 15:14:19 +0000

    camprision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);           // avoid this type of camprision
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === strickly check value
+
+console.log("2" === 2);
