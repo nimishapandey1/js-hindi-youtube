9fc167d3b530314e2225c8e60143a5202c1efe59
nimishapandey1 <144951514+nimishapandey1@users.noreply.github.com>
2023-10-07 14:58:33 +0000

    conversionoperation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -29,3 +29,25 @@ let someNumber = 33
 let stringNumber =  String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//***************operation ************* 
+
+let value = 3
+let negvalue =  - value
+console.log(negvalue);
+
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" +2 + 2);
+console.log(1+2+"2");
+
+console.log((3+4) *5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3 = 2+2
