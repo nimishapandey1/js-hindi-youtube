9e6ee67710104f969db112c86ec5dac9cbdcb439
nimishapandey1 <144951514+nimishapandey1@users.noreply.github.com>
2023-10-08 04:44:55 +0000

    datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+// Primitive 
+
+// 7types : String, Number, Boolean, null, Undefined, symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const BigNumber = 1234567899876543221n
+
+
+// Reference type(Nom Primitive)
+
+// Array, Objects, Function
+
